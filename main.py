2d18766e9b636d3545f90877bfaa8688aa00648f
baltimoreace <baltimoreace@gmail.com>
2025-02-15 21:52:13 -0500

    Bookbot Report

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ with open("books/frankenstein.txt") as f:
     text = f.read()
     lowered_text = text.lower()
 
+def sort_on(character_count):
+    return character_count["num"]
 
 def main():
+    
+    words = text.split()
+    word_count = len(words)
+        
+    
     character_count = {}
     for characters in lowered_text:
         if characters in character_count:
@@ -11,8 +18,20 @@ def main():
         else:
             character_count[characters] = 1
 
-    print(character_count)
+    char_list = []
+    for char, count in character_count.items():
+        if char.isalpha():
+            char_list.append({"char": char, "num": count})
+    char_list.sort(reverse=True, key=sort_on)
 
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    
+    for item in char_list:
+        print(f"The '{item['char']}' character was found {item['num']} times")
+    
+    print("--- End report ---")
            
 if __name__ == "__main__":
     main()
